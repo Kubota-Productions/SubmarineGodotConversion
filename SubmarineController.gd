5ceb53be3a29074cb43d63831b9a6d1eb1079108
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2025-03-15 22:19:08 +0900

    upload

--- SubmarineController.gd
+++ SubmarineController.gd
@@ -5,61 +5,51 @@ extends RigidBody3D
 @export var curve : Curve ## Unused for now
 @export var thrust : float = 0.10 ## default "engine on" speed
 @export var max_thrust : float  = 0.30 ## The "boost speed"
-@export_range(-100.0, 100.0) var y_axis_thrust : float = 0.0 ## Thrust along the vertical axis, locally relative to the sub
-@export_range(-100.0, 100.0) var x_axis_thrust : float = 0.0 ## Thrust along the left-right axis, locally relative to the sub
-@export var turn_torque: Vector3 = Vector3(90.0, 25.0, 45.0) ## Don't understand yet
-@export var force_mult: float = 1000.0 ## Don't understand yet
-@export var sensitivity: float = 5.0 ## Don't understand yet
-@export var aggressive_turn_angle: float = 10.0	## Maybe means maximum turn angle, unsure
+@export var turn_torque: Vector3 = Vector3(90.0, 25.0, 45.0) 
+@export var force_mult: float = 1000.0 
+@export var sensitivity: float = 5.0 
+@export var aggressive_turn_angle: float = 10.0 
 @export var controller: MouseFlightController ## Camera Rig Node
 
-var pitch: float = 0.0 ## Rotation around the x-axis
-var yaw: float = 0.0 ## Rotation around the y-axis
-var roll: float = 0.0 ## Rotation around the z-axis
-var engine_toggle: bool = false ## The engine being on or off
-var previous_speed: float = 0.0 ## necessary for acceleration and deceleration math
-var current_thrust: float = 0.0 ## Don't understand yet
-var move_direction: Vector3 ## May be unintuitive.
-
-#func _ready():
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)		## Disables the mouse from exiting the viewport
-	## TODO https://docs.godotengine.org/en/4.3/classes/class_input.html#enum-input-mousemode
-	## may want to change the input of the mouse to MOUSE_MODE_CAPTURED, but the way to collect mouse
-	## movement changes to InputEventMouseMotion.relative   // I would just implement this myself 
-	## but I'm not there yet, hence: TODO
+var pitch: float = 0.0 
+var yaw: float = 0.0 
+var roll: float = 0.0 
+var engine_toggle: bool = false 
+var previous_speed: float = 0.0 
+var current_thrust: float = 0.0 
+var move_direction: Vector3 
 
 func _process(delta):
-	## captures the input for y axis and x axis movement RELATIVE TO THE SUB'S ROTATION
-	var vertical_movement = Input.get_axis("Backward", "Forward")
+	## Get input for movement
+	var vertical_movement = Input.get_axis("Backward", "Forward") ## Now controls vertical movement
 	var horizontal_movement = Input.get_axis("Left", "Right")
 	
-	## captures the input for engaging and disengaging the engine
+	## Engine toggle
 	if Input.is_action_just_pressed("toggle_thrust"):
-		engine_toggle = not engine_toggle # taggles to the opposite state
+		engine_toggle = !engine_toggle 
 	
-	## if the engine is ON...
+	## Adjust speed based on engine state
 	if engine_toggle:
-		## change target speed to standard thrust value
 		change_speed(thrust, delta)
-		## if the speed boost is activated...
 		if Input.is_action_pressed("speed_boost"):
-			## change target speed to max_thrust value
 			change_speed(max_thrust, delta)
 	else:
 		change_speed(0.0, delta)
-	
+
+	## Auto-turn behavior
 	var auto_yaw = 0.0
 	var auto_pitch = 0.0
 	var auto_roll = 0.0
 
-	if controller: 
+	if controller:
 		run_autopilot(controller.MouseAimPos(), auto_yaw, auto_pitch, auto_roll)
 
 	yaw = auto_yaw
 	pitch = auto_pitch
 	roll = auto_roll if not Input.is_action_pressed("manual_roll") else horizontal_movement
 	
-
+	## Ensure submarine moves forward when thrust is toggled
+	move_direction = (transform.basis * Vector3(0, vertical_movement, 1)).normalized() * current_thrust * force_mult
 
 func change_speed(speed: float, delta: float):
 	if !is_equal_approx(previous_speed, speed):
@@ -83,10 +73,9 @@ func run_autopilot(fly_target: Vector3, out_yaw: float, out_pitch: float, out_ro
 	out_roll = lerp(wings_level_roll, aggressive_roll, wings_level_influence)
 
 func _physics_process(delta):
-	move_direction = (Vector3.UP * y_axis_thrust * force_mult) + (Vector3.RIGHT * x_axis_thrust * force_mult)
-	
-
-	apply_central_force((Vector3.FORWARD * current_thrust * force_mult + move_direction) * 1)
+	## Apply movement forces based on thrust
+	apply_central_force(move_direction)
 	apply_torque(Vector3(turn_torque.x * pitch, turn_torque.y * yaw, -turn_torque.z * roll) * force_mult)
+	
 	print("current pos = " + str(position))
 	print("vel = " + str(linear_velocity))

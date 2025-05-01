c36b5d5d2e8268ec31d1a5dcf4a6067dc5c25eb2
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2025-05-01 11:54:42 +0900

    update

--- Scripts/Hud.gd
+++ Scripts/Hud.gd
@@ -4,6 +4,10 @@ extends Control
 @export var boresight: Control = null
 @export var mouse_pos: Control = null
 @export var player_cam: Camera3D = null
+@export var submarine: Node3D = null
+@export var dial: Control = null
+
+
 
 func _ready():
 	# Ensure HUD spans the entire viewport
@@ -36,9 +40,9 @@ func _find_camera_in_node(node: Node) -> Camera3D:
 func _process(_delta: float) -> void:
 	if mouse_flight == null or player_cam == null:
 		return
-	update_graphics(mouse_flight)
+	update_graphics(_delta, mouse_flight)
 
-func update_graphics(controller: Node) -> void:
+func update_graphics(delta, controller: Node) -> void:
 	if not (controller and controller.has_method("get_boresight_pos") and controller.has_method("get_mouse_aim_pos")):
 		return
 	
@@ -50,8 +54,10 @@ func update_graphics(controller: Node) -> void:
 	if mouse_pos:
 		var mouse_aim_world_pos: Vector3 = controller.get_mouse_aim_pos()
 		var mouse_pos_screen_pos: Vector2 = player_cam.unproject_position(mouse_aim_world_pos)
-		mouse_pos.position = mouse_pos_screen_pos - mouse_pos.size / 2  # Center the node
-
+		dial.rotation = deg_to_rad(submarine.transform.basis.z.z) * 20
+		
+		
+		
 func set_reference_mouse_flight(controller: Node) -> void:
 	mouse_flight = controller
 	player_cam = _find_camera_in_node(mouse_flight) if mouse_flight != null else null
